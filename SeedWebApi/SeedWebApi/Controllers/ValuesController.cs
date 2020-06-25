8ff0853af851a3ad5cf51f656e94acd8a92f1314
vramanavar <vramanavar@gmail.com>
2020-06-25 20:09:24 +0530

    Added two more fruits to api/values endpoint

--- SeedWebApi/SeedWebApi/Controllers/ValuesController.cs
+++ SeedWebApi/SeedWebApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SeedWebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Apple", "Banana", "Orange", "Avocado" };
+            return new string[] { "Apple", "Banana", "Orange", "Avocado", "Custard Apple", "Pomegranate" };
         }
 
         // GET api/values/5
